b04f77628137aeff623b98abcc165221a1f0e3b3
stefania <stephy.maria12@gmail.com>
2020-09-14 12:15:33 +0300

    temp design

--- components/screens/DishDetails.tsx
+++ components/screens/DishDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { NavigationTabScreenProps } from 'react-navigation-tabs';
 import * as basic from '../basic';
 import * as basicStyles from '../basic/styles';
@@ -8,7 +8,9 @@ interface SettingsScreenProps extends NavigationTabScreenProps {
 }
 
 const dish = {
+  image:"https://www.natalieshealth.com/wp-content/uploads/2019/01/Blueberry-Oatmeal-Porridge-3892.jpg",
   title: 'Oatmeal Porridge',
+  nutriInfo:"Nutritional Info",
   description: `Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. In the 2010s, the sending of short informal messages has become an accepted part of many cultures, as happened earlier with emailing.[1] This makes texting a quick and easy way to communicate with friends, family and colleagues, including in contexts where a call would be impolite or inappropriate (e.g., calling very late at night or when one knows the other person is busy with family or work activities). Like e-mail and voicemail and unlike calls (in which the caller hopes to speak directly with the recipient), texting does not require the caller and recipient to both be free at the same moment; this permits communication even between busy individuals. Text messages can also be used to interact with automated systems, for example, to order products or services from e-commerce websites, or to participate in online contests. Advertisers and service providers use direct text marketing to send messages to mobile users about promotions, payment due dates, and other notifications instead of using postal mail, email, or voicemail.`
 }
 
@@ -17,11 +19,12 @@ class SettingsScreen extends React.Component<SettingsScreenProps> {
     return (
       <basic.Screen>
         <View style={styles.dish}>
-          <View style={styles.image}>
-            <basic.CustomText label={'IMAGE'} />
-          </View>
-          <View style={styles.header}>
+          <Image style={styles.image} source={{ uri: dish.image }} />
+          <View style={styles.title}>
             <basic.CustomText label={dish.title} />
+          </View>
+          <View style={styles.nutriInfo}>
+            <basic.CustomText label={dish.nutriInfo} />
           </View>
           <View style={styles.description}>
             <ScrollView>
@@ -39,21 +42,27 @@ export default SettingsScreen;
 const styles = StyleSheet.create({
   dish: {
     flex: 1,
-    paddingLeft:12,
-    paddingRight:12,
-    paddingTop:12,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 12,
     backgroundColor: basicStyles.ACCENT_YELLOW
   },
   image: {
     flex: 2,
     alignItems: 'center',
     backgroundColor: basicStyles.ACCENT_RED,
-    borderRadius:12
+    borderRadius: 12,
+    height: '100%',
+    width: '100%'
   },
-  header: {
+  title: {
+    flex: 1,
+    backgroundColor: basicStyles.ACCENT_BLUE,
+    marginTop: 10
+  },
+  nutriInfo:{
     flex: 2,
-    alignItems: 'center',
-    backgroundColor: basicStyles.ACCENT_BLUE
+    backgroundColor: basicStyles.ACCENT_RED,
   },
   description: {
     flex: 3,
--- components/screens/home/DishList.tsx
+++ components/screens/home/DishList.tsx
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
     backgroundColor: basicStyles.BACKGROUND_MEDIUM,
   },
   image: {
-    width: 100, 
+    width: 100,
     height: 100
   },
   itemList: {
